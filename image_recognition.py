68f6fb7fd2e63777a5c15533e7d7b5f78de3a7e5
m-malikov <malikov.ma@phystech.edu>
2017-10-08 10:11:51 +0300

    Fixed bug with rating

--- image_recognition.py
+++ image_recognition.py
@@ -29,11 +29,14 @@ def find_info(bar_code):
     if title.strip() == u'Гудс Матрикс':
         return None, None, None
     mark = soup.find('span', id="ctl00_ContentPH_Mark_MarkL")
-    reviews_score = db.get_score(title.split()[2])
-    if mark is not None:
-        mark = (float(mark.string) + reviews_score)/2
-    else:
+    reviews_score = float(db.get_score(title.split()[2]))
+    print(reviews_score)
+    if mark.string is not None and reviews_score is not None:
+        mark = (float(mark.string.split()[0].replace(',', '.'))/2 + reviews_score)/2
+    elif mark is not None:
         mark = reviews_score
+    elif reviews_score is not None:
+        mark = float(mark.string.split()[0].replace(',', '.'))/2
     mark_num = soup.find('span', id="ctl00_ContentPH_Mark_MarkNum")
     if mark_num is not None:
         mark_num = mark_num.string
